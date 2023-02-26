cd886ca747cc49d98d9c6774724e35988b5b22ea
David Smit'h <115672052+FDavidSmith@users.noreply.github.com>
2023-02-26 19:00:04 -0300

    se agrega localStorage

--- JS/admin.js
+++ JS/admin.js
@@ -1,4 +1,5 @@
 const table = document.getElementById('table-game');
+let listaJuegos = [];
 
 function validacion (){
     if(document.getElementById('recipient-code').value!==''){
@@ -13,40 +14,64 @@ function validacion (){
 }
 
 function agregarJuego (){
-    const datos = document.querySelectorAll('.dato');
-    if(validacion()){
+    let filas = document.querySelectorAll('.fil');
+    for(fila of filas){
+        fila.innerHTML='';
+    }    
+if(validacion()){
+    let juego = {
+        codigo : document.getElementById('recipient-code').value,
+        nombre : document.getElementById('recipient-name').value,
+        category : document.getElementById('recipient-category').value,
+        descripcion : document.getElementById('message-text').value,
+        publicado : document.getElementById('published').value
+    };
+    listaJuegos.push(juego);
+    localStorage.setItem('juegos',JSON.stringify(listaJuegos));
+
+    let juegosGuardados = JSON.parse(localStorage.getItem('juegos'));
+    for (juego of juegosGuardados){
+        let cod = juego.codigo;
+        let name = juego.nombre;
+        let cat = juego.category;
+        let descr = juego.descripcion;
+        let pub = juego.publicado;
+        let campos = [cod,name,cat,descr,pub];
+
         let row = document.createElement('tr');
-        row.id = document.getElementById('recipient-code').value;
+        row.id = parseInt(juego.codigo);
+        row.className ='fil';
         let botonEliminar = document.createElement('button');
-        botonEliminar.id = document.getElementById('recipient-code').value;
+        botonEliminar.id = parseInt(juego.codigo)+1;
         botonEliminar.onclick = ()=>{
-            if(botonEliminar.id!== ''&& botonEliminar.id === row.id){
+            if(botonEliminar.id !== '' && ((botonEliminar.id)-1) === parseInt(row.id)){
                 row.innerHTML ='';
             }
         }
-        let botonEditar = document.createElement('button');
+        let botonEditar = document.createElement('div');
+        botonEditar.innerHTML = '<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo"><i class="fa-solid fa-pen-to-square text-light fs-4"></i></button>'
+        botonEditar.style = "width: 48px"
         let botonDestacar = document.createElement('button');
         botonEliminar.innerHTML = '<i class="fa-solid fa-trash text-light fs-4"></i>'
-        botonEditar.innerHTML = '<i class="fa-solid fa-pen-to-square text-light fs-4"></i>'
         botonDestacar.innerHTML = '<i class="fa-regular fa-star text-light fs-4"></i>'
-        botonEliminar.style.border = 'none'
-        botonEditar.style.border = 'none'
-        botonDestacar.style.border = 'none'
+        botonEliminar.style.border = 'none';
+        botonEditar.firstChild.style.border = 'none';
+        botonDestacar.style.border = 'none';
         table.appendChild(row);
-        for (dato of datos){
-            let campo = dato.value;
+
+        for (val of campos){
             let tableData = document.createElement('td');
-            tableData.textContent = campo;
+            tableData.textContent = val;
             tableData.style = 'color:white; text-align:center'
             row.appendChild(tableData);
+            }
+            let opciones = document.createElement('td');
+            opciones.classList = 'd-flex justify-content-center'
+            opciones.append(botonEliminar,botonEditar,botonDestacar);
+            row.appendChild(opciones);
         }
-        let opciones = document.createElement('td');
-        opciones.style.textAlign = 'center';
-        opciones.append(botonEliminar,botonEditar,botonDestacar);
-        row.appendChild(opciones);
-        for(dato of datos){
-            dato.value = '';
-        }
+        
+    
     } else {
         alert('Los campos son obligatorios');
     }
