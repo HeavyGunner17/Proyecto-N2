6ad6c1002cd4d5aa767e716bd0e589b2d906293e
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-02-28 20:13:41 -0300

    se agrego formulario en index y se agrega validacion en js.

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,29 @@
-function redirect(){
-    return console.log("holapino")
+
+function redirect(moveTo) {
+    switch (moveTo) {
+        case "admin":
+            window.location.href = "../Paginas/administración.html"
+            break;
+        case "user":
+            window.location.href = "../index.html"
+
+            break;
+
+        default:
+            break;
+    }
 }
+
+function validate(event) {
+    const isValidMail = document.getElementById("username").value
+    const isValidPass = document.getElementById("password").value
+    event.preventDefault()
+    if (isValidMail == "admin" && isValidPass == "admin123") {
+        redirect("admin")
+    } else {
+        redirect("user")
+        console.log("else")
+    }
+}
+
+
