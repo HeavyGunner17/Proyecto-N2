e21c23d835b5f976a5b336f2b0d72f066716eb78
Rodrigo-Brizuela <rodrigo8935@gmail.com>
2023-03-02 19:54:56 -0300

    Toma de datos en adminUsers

--- JS/app_adminUsers.js
+++ JS/app_adminUsers.js
@@ -6,7 +6,7 @@ En editar abre modal para dar de alta el usuario, suspenderlo, o dejarlo pendien
 
 /* ESTRUCTURA DE FILAS EN LA TABLA
 <tr>
-    <th class="table_text" scope="row">1</th>
+    <td class="table_text" scope="row">1</td>
     <td class="table_text">${userRegistred[0]}</td>
     <td class="table_text">${userRegistred[1]}</td>
     <td class="table_text">${userRegistred[2]}</td>
@@ -19,21 +19,62 @@ En editar abre modal para dar de alta el usuario, suspenderlo, o dejarlo pendien
 </tr>
 */
 
+let usuario = {
+    id : 1,
+    name : "Rodrigo Brizuela",
+    email : "rodrigo8935@gmail.com",
+    status: "Pendiente"
+}
+
+let usuariosArray = [usuario]
+
 const table = document.getElementById('table_user');
 
-let usuarios = ["Rodrigo Brizuela, rodrigo8935@gmail.com, Pendiente", "Macarena Correa, macacorrea715@gmail.com, Pendiente", "Roberto Juarez, rojuarez@gmail.com, Pendiente"]
 
-localStorage.setItem('user', usuarios);
+localStorage.setItem('user', JSON.stringify(usuariosArray));
 
 
 function loadUsers () {
-    let userRegistred = localStorage.getItem('user').split(',');
-    
+    let userRegistred = JSON.parse(localStorage.getItem('user'));
+
+
+    for (user of userRegistred){
+        let row = document.createElement('tr');
+        table.appendChild(row)
+
+        let id = user.id;
+        let name = user.name;
+        let email = user.email;
+        let status = user.status;
+
+        let userData = [id, name, email, status];
+
+        for (dato of userData){
+            let campo = document.createElement('td');
+            campo.className= "table_text";
+
+            campo.textContent = dato;
+
+            row.appendChild(campo);
+            
+        }
+
+        let options = document.createElement('td');
+        options.className = "table_text";
+        options.innerHTML += `<button class="bg-transparent border-0" data-bs-toggle="modal" data-bs-target="#editUserModal">
+        <i class="fa-solid fa-user-pen icon_userEdit">
+        </i>
+      </button>`
+
+      row.appendChild(options);
+        
+    }
 
 }
 
 function statusChange () {
     const newStatus = document.getElementById('statusUser').value;
+
 
 
 
