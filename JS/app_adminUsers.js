0293bd382262663c3fee13fab39bf8169f2822f7
rodrigo <rodrigo8935@gmail.com>
2023-03-02 11:38:33 -0300

    Modal editar usuario creado, falta logica JS

--- JS/app_adminUsers.js
+++ JS/app_adminUsers.js
@@ -7,16 +7,36 @@ En editar abre modal para dar de alta el usuario, suspenderlo, o dejarlo pendien
 /* ESTRUCTURA DE FILAS EN LA TABLA
 <tr>
     <th class="table_text" scope="row">1</th>
-    <td class="table_text">Rodrigo</td>
-    <td class="table_text">rodrigo8935@gmail.com</td>
-    <td class="table_text">Pendiente</td>
-    <td class="table_text"><a href=""><i class="fa-solid fa-user-pen icon_userEdit"></i></a></td>
+    <td class="table_text">${userRegistred[0]}</td>
+    <td class="table_text">${userRegistred[1]}</td>
+    <td class="table_text">${userRegistred[2]}</td>
+    <td class="table_text">
+      <button class="bg-transparent border-0" data-bs-toggle="modal" data-bs-target="#editUserModal">
+        <i class="fa-solid fa-user-pen icon_userEdit">
+        </i>
+      </button>
+    </td>
 </tr>
 */
 
 const table = document.getElementById('table_user');
 
+let usuarios = ["Rodrigo Brizuela, rodrigo8935@gmail.com, Pendiente", "Macarena Correa, macacorrea715@gmail.com, Pendiente", "Roberto Juarez, rojuarez@gmail.com, Pendiente"]
+
+localStorage.setItem('user', usuarios);
+
 
 function loadUsers () {
+    let userRegistred = localStorage.getItem('user').split(',');
     
+
 }
+
+function statusChange () {
+    const newStatus = document.getElementById('statusUser').value;
+
+
+
+}
+
+loadUsers();
