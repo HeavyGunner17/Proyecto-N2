210ba92b7836177a2759920fa202b647d89768ae
FacundoAguirre7 <122408290+FacundoAguirre7@users.noreply.github.com>
2023-03-02 19:32:55 -0300

    se code sobre footer y nosotros

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,6 +1,11 @@
-const nombre=document.getElementById("nombre");
 
 function mostrar(){
-    console.log(nombre.value);
-}
+    const nombre=document.getElementById("nombre");
+    const name=nombre.value;
+
+    console.log(name);
+};
+
 mostrar();
+const enviar=document.getElementById("enviar");
+enviar.onclick=mostrar
