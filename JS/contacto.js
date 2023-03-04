aadad53d074b353f1ea8d812e61783f8eed1a89a
FacundoAguirre7 <122408290+FacundoAguirre7@users.noreply.github.com>
2023-03-04 13:05:43 -0300

    js en contacto

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,11 +1,45 @@
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const contacto = document.getElementById("comentario");
 
-function mostrar(){
-    const nombre=document.getElementById("nombre");
-    const name=nombre.value;
+const datos = [nombre,email,contacto];
 
-    console.log(name);
-};
+function validacion (){
+if(nombre.value !== "" && email.value !== ""|| mail() && contacto.value !== ""){
+    alert("mensaje enviado");
+    //funcion de envio de mail
+    for(dato of datos){
+        dato.value="";
+    }
 
-mostrar();
-const enviar=document.getElementById("enviar");
-enviar.onclick=mostrar
+}else{
+    alert("Algun campo incompleto");
+
+}
+
+}
+
+
+
+function mail (){
+    if(!email.value.includes("@")){
+        alert("email incorrecto");
+        email.focus();
+        return true;
+    }
+}
+
+
+
+
+
+
+
+// for(dato of datos) {
+//     if(dato.value !== ""){
+//         alert("este campo el obligatorio");
+//         dato.focus();
+//     }else{
+//         alert("mensaje enviado");
+//     }
+// }
