efab160610ed69f80133553ed170d5a580f8e00d
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-03-05 21:03:24 -0300

    se agregan funciones al registro y se guardan los users en el localst

--- JS/app.js
+++ JS/app.js
@@ -14,15 +14,40 @@ function redirect(moveTo) {
     }
 }
 
-function validate(event) {
-    const isValidMail = document.getElementById("username").value
-    const isValidPass = document.getElementById("password").value
-    event.preventDefault()
-    if (isValidMail == "admin" && isValidPass == "admin123") {
-        redirect("admin")
+function validate(event, type) {
+    if (type == "login") {
+        const isValidMail = document.getElementById("username").value
+        const isValidPass = document.getElementById("password").value
+        event.preventDefault()
+        if (isValidMail == "admin" && isValidPass == "admin123") {
+            redirect("admin")
+        } else {
+            redirect("user")
+            console.log("else")
+        }
+    } else if (type == "create") {
+
+        event.preventDefault()
+        const newUser = document.getElementById("newUsername").value
+        const newUserPass = document.getElementById("newPassword").value
+        const newUserPassConfirm = document.getElementById("confirm").value
+        const newMail = document.getElementById("userEmail").value
+        if (newUserPass == newUserPassConfirm) {
+            const user = [
+                {
+                    user: newUser,
+                    mail: newMail,
+                    password: newUserPass,
+                }
+            ]
+            const userJSON = JSON.stringify(user)
+            localStorage.setItem('user', userJSON)
+            console.log(localStorage.getItem("user"))
+        } else {
+            alert("The passwords are different")
+        }
     } else {
-        redirect("user")
-        console.log("else")
+        alert("There has been an error.")
     }
 }
 
