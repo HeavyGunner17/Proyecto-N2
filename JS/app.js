014c7b997c29062a6363da8864e23676180e265e
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-03-07 00:14:17 -0300

    se modifico  loaduser, se agrego funcion para validar previamente, y logout

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,65 @@
+function validatePreviousLogin() {
+    const getloggeduser = localStorage.getItem('loggedUser')
+    const loggeduser = JSON.parse(getloggeduser)
+    if (localStorage.getItem('loggedUser')) {
+        document.getElementById('navbar-login').innerHTML = `
+        <div class="dropdown">
+        <a class="nav-link text-white dropdown-toggle" data-bs-toggle="dropdown">${loggeduser.user}</a>  
+        <ul class="dropdown-menu">
+        <li class="dropdown-user"><a class="dropdown-item" href="#">View Profile</a></li>
+        <li class="dropdown-user"><a class="dropdown-item" href="#" onclick="logout()">Logout</a></li>
+        </ul>
+        </div>`
+    } else {
+        document.getElementById('navbar-login').innerHTML = `
+        <a class="nav-link text-white" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</a>`
+    }
+}
+validatePreviousLogin()
+
 function loadUsers() {
     const users = [{
+        id: 1,
         username: "rodri8935",
         email: "rodrigo8935@gmail.com",
-        password: "rodrigo123"
+        password: "rodrigo123",
+        estado: "pendiente"
     }, {
+        id: 2,
         username: "macacorrea715",
         email: "macacorrea715@gmail.com",
-        password: "maca715unodos"
+        password: "maca715unodos",
+        estado: "pendiente"
     }, {
+        id: 3,
         username: "liomessi",
         email: "liomessi@gmail.com",
-        password: "quemirabobo"
+        password: "quemirabobo",
+        estado: "pendiente"
+    },
+    {
+        id: 4,
+        username: "robert17",
+        email: "robert17@hotmail.com",
+        password: "robert96",
+        estado: "aprobado"
+    },
+    {
+        id: 5,
+        username: "pepino25",
+        email: "pepipes@gmail.com",
+        password: "pepino98",
+        estado: "aprobado"
+
     }]
 
     const usersToLoad = JSON.stringify(users)
+
     localStorage.setItem('users', usersToLoad)
     console.log(localStorage.getItem('users'), 'datos localstorage')
 }
 
 loadUsers()
-
-
 
 
 
@@ -45,28 +84,38 @@ function validate(event, type) {
         let userList = JSON.parse(userListJSON)
         event.preventDefault()
         if (isValidMail == "admin" && isValidPass == "admin123") {
+            const loggedIn = {
+                user: isValidMail,
+            }
+            const loggedInUser = JSON.stringify(loggedIn)
+            localStorage.setItem('loggedUser', loggedInUser)
             redirect("admin")
         }
         else if (userList.find(({ username }) => username === isValidMail)) {
             const enteredUser = userList.find(({ username }) => username === isValidMail)
-            console.log(enteredUser)
-            if ((enteredUser.username == isValidMail && enteredUser.password == isValidPass) || (isValidMail == enteredUser.email && isValidPass == enteredUser.password)) {
-                alert('Login exitoso como usuario')
+            if ((enteredUser.username == isValidMail && enteredUser.password == isValidPass && enteredUser.estado == 'aprobado')) {
+                const loggedIn = {
+                    user: isValidMail,
+                }
+                const loggedInUser = JSON.stringify(loggedIn)
+                localStorage.setItem('loggedUser', loggedInUser)
+
+                alert('Successful login')
                 redirect('user')
             } else {
-                alert('mal ingreso de contraseña')
+                alert('Wrong Password or user is not allowed')
             }
         }
         else {
-            alert('there has been an error while signing in')
+            alert('The user does not exist')
         }
     }
     else if (type == "create") {
         event.preventDefault()
-        const newUser = document.getElementById("newUsername").value
-        const newUserPass = document.getElementById("newPassword").value
-        const newUserPassConfirm = document.getElementById("confirm").value
-        const newMail = document.getElementById("userEmail").value
+        let newUser = document.getElementById("newUsername").value
+        let newUserPass = document.getElementById("newPassword").value
+        let newUserPassConfirm = document.getElementById("confirm").value
+        let newMail = document.getElementById("userEmail").value
         if (newUserPass == newUserPassConfirm) {
             const user =
             {
@@ -79,11 +128,13 @@ function validate(event, type) {
             userList.push(user)
             let newUserList = JSON.stringify(userList)
             localStorage.setItem('users', newUserList)
-            
-            const myModal = new bootstrap.Modal('#exampleModal')
-            console.log(myModal)
-            myModal.dispose()
-            alert('usuario creado con exito')
+
+            alert('User creation was successful')
+            newUser = document.getElementById("newUsername").value = ''
+            newUserPass = document.getElementById("newPassword").value = ''
+            newUserPassConfirm = document.getElementById("confirm").value = ''
+            newMail = document.getElementById("userEmail").value = ''
+            document.getElementById('closeModal').click()
         } else {
             alert("The passwords are different")
         }
@@ -93,3 +144,7 @@ function validate(event, type) {
 }
 
 
+function logout(){
+    localStorage.removeItem('loggedUser')
+    location.reload()
+}
