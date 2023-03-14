1a3c9a7a2cfa95cd70e5d1618bf1a8bb8330a407
HeavyGunner17 <heavygunner17alv@gmail.com>
2023-03-14 14:46:24 -0300

    se agrega al formulario las preguntas de seguridad y respuesta  y seagrega la visibilidad de la pregunta

--- JS/recoverPassword.js
+++ JS/recoverPassword.js
@@ -1,8 +1,10 @@
-
 let users = JSON.parse(localStorage.getItem('users'))
 console.log(users)
 
-
+function getSecurityQuestion(user) {
+    console.log(user.securityQuestion)
+    return user.securityQuestion
+}
 function changeUserPassword() {
     event.preventDefault()
     let usernameInput = document.getElementById('username').value
@@ -25,18 +27,19 @@ function changeUserPassword() {
     console.log(JSON.parse(localStorage.getItem('users')))
     alert('Password succesfully changed')
 }
-
-
 function validateInput() {
     let usernameInput = document.getElementById('username').value
     let questionInput = document.getElementById('securityQuestion').value
     let passwordInput = document.getElementById('password').value
     let users = JSON.parse(localStorage.getItem('users'))
     const foundUser = users.find(({ username }) => username == usernameInput)
-    if (foundUser.username == usernameInput && foundUser.securityAnswer == questionInput) {
+    if (foundUser.username == usernameInput) {
         document.getElementById('password').disabled = false
         document.getElementById('password-change').disabled = false
+        document.getElementById('securityQuestion').disabled = false
         alert('Correctly Validated')
+        let userSecurityQuestion = getSecurityQuestion(foundUser)
+        document.getElementById('security-question-label').innerHTML = `${userSecurityQuestion}`
     } else {
         alert('You are changing your password.')
     }
