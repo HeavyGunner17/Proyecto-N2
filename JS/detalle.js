f79508b23221f3594e519a7205763b6cf58abb6a
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2023-03-16 17:10:15 -0300

    Pagina Detalle y funciones para mostrar

--- JS/detalle.js
+++ JS/detalle.js
@@ -1,44 +1,100 @@
-const verMas = document.getElementById('detalleid');
-const car = document.getElementById('detail-template');
 
-let nombre="";
-let description="";
-let background_image="";
-
-let website ="";
-let rating="";
-let platforms=[];
-
-async function detalle(id){
-    let valor= id;
-  console.log(valor);
-  //console.log("hola desde otro lado"+ valor);
-  const api= `https://api.rawg.io/api/games/${id}?key=85458154fdcb4c3abf3290a579a65e18`
-       const res = await fetch(api);
-        const json = await res.json();
-
-       // console.log("imprimo RES",res);
-    //console.log("imprimo JSON",json.description);
-    //window.location.href="./detalle.html"
-             // car.innerHTML="";
-          
-         
-    
-    nombre =json.name;
-    description =json.description;
-    background_image =json.background_image;
-   
-    website = json.website;
-    rating= json.rating;
-    
-    //console.log(nombre, description);
-  console.log(website);
-
-//window.location.href="./detalle.html";
-//displayGames2();
+const prueba =JSON.parse(localStorage.getItem("detail"))
+/**
+ * console.log(localStorage.getItem('detail'))
+console.log("prueba");
+console.log(prueba);
+console.log(typeof prueba); 
+ * 
+ */
 
 
+async function detalle(prueba){
+  let valor=localStorage.getItem('detail');
 
+const api= `https://api.rawg.io/api/games/${valor}?key=85458154fdcb4c3abf3290a579a65e18`
+//verfico que valor llega a la api
+  //console.log(api);
+     const res = await fetch(api);
+      const json = await res.json();
+      const modal = document.getElementById('detail-template');
+     // console.log("imprimo RES",res);
+  //console.log("imprimo JSON",json.description);
+  
+        modal.innerHTML="";
+        modal. innerHTML+=`
+        <div class="row g-0">
+        <div class="col-md-4">
+          <img
+          src="${json.background_image}" 
+          alt="${json.name}"     
+          class="img-fluid mt-4"
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h2 class="card-title fw-bold">${json.name}</h2>
+            <p class="card-text">${json.description}</p>
+            <p class="card-text">
+            <p>Released: <span class="fw-bold">${json.released}</span> </p>
+              
+             <p> Rating: <span class="fw-bold">${json.rating}</span></p>
+            </p>
+            <div>
+            <a class="btn btn-success" href="./404.html"><i class="fa-sharp fa-solid fa-cart-shopping"> Shop Now!</i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+        `
 }
+
+//imagenes
+const getImages = async ()=>{
+let valor=localStorage.getItem('detail');
+const res = await  fetch(`https://api.rawg.io/api/games/${valor}/screenshots?key=85458154fdcb4c3abf3290a579a65e18`)
+const json = await res.json()
+//console.log(json.results);
+const containercards = document.querySelector('#container-cards')
+containercards.innerHTML=""
+json.results.map((index)=>{
+const col = document.createElement('div');
+const tarjeta = `
+<div class="p-2">
+<img src="${index.image}" class="w-100" >
+</div>
+`
+col.innerHTML=tarjeta;
+containercards.append(col);
+})
+}
+
+//videos
+const getMovies = async()=>{
+  const res = await  fetch(`https://api.rawg.io/api/games/${localStorage.getItem('detail')}/movies?key=85458154fdcb4c3abf3290a579a65e18`)
+  const json = await res.json()
+  //console.log(json.results);
+  const videos = document.querySelector('#videos-container')
+  videos.innerHTML=""
+  json.results.map((video)=>{
+    const col = document.createElement('div');
+    col.className="col";
+    const tarjeta = `
+    <div class="card " >
+    <video src=${video.data.max} controls>
+    </div>
+    `
+    col.innerHTML=tarjeta;
+    videos.append(col);
+  })
+}
+
+
+detalle();
+getImages();
+getMovies()
+
+
+
 
 
